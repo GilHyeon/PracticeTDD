48c38abd9c0f309530c9651543f9cfc2cedd086c
GilHyeon <honggh34@gmail.com>
2017-12-02 17:01:46 +0900

    Money - chapter 15

--- Money/src/test/java/MoneyTest.java
+++ Money/src/test/java/MoneyTest.java
@@ -64,13 +64,23 @@ public class MoneyTest {
         assertEquals(Money.dollar(1), result);
     }
 
-    @Test
+    /*@Test
     public void testArrayEquals() {
         assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
-    }
+    }*/
 
     @Test
     public void testIdentityRate() {
         assertEquals(1, new Bank().rate("USD", "USD"));
+    }
+
+    @Test
+    public void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
     }
 }
