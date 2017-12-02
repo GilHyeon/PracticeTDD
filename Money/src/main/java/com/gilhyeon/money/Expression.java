d5c8f34e6b34cfd62eb58bb12d02bd4899229b5e
GilHyeon <honggh34@gmail.com>
2017-12-02 16:20:02 +0900

    Money - chapter 13

--- Money/src/main/java/com/gilhyeon/money/Expression.java
+++ Money/src/main/java/com/gilhyeon/money/Expression.java
@@ -1,4 +1,5 @@
 package com.gilhyeon.money;
 
 public interface Expression {
+    Money reduce(String to);
 }
--- Money/src/test/java/MoneyTest.java
+++ Money/src/test/java/MoneyTest.java
@@ -1,6 +1,7 @@
 import com.gilhyeon.money.Bank;
 import com.gilhyeon.money.Expression;
 import com.gilhyeon.money.Money;
+import com.gilhyeon.money.Sum;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -36,5 +37,22 @@ public class MoneyTest {
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
+    }
+
+    @Test
+    public void testPlusReturnsSum() {
+        Money five = Money.dollar(5);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
+        assertEquals(five, sum.augend);
+        assertEquals(five, sum.addend);
+    }
+
+    @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum,"USD");
+        assertEquals(Money.dollar(7), result);
     }
 }
